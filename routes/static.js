9d9c43372b5d8f7fd0085bc428b2e96e72d932f1
tartasprint <tartasprint.dev@gmail.com>
2022-01-04 13:58:36 +0100

    Changed katex to be served from node_modules

--- routes/static.js
+++ routes/static.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 // define the home page route
 router.use('/', express.static('dist'))
+router.use('/katex', express.static('node_modules/katex/dist'))
 router.use('/bootstrap', express.static('node_modules/bootstrap/dist'))
 router.use('/popper', express.static('node_modules/@popperjs/core/dist'))
 router.use('/jquery', express.static('node_modules/jquery/dist'))
@@ -13,7 +14,6 @@ router.use('/editorjs/marker.js', express.static('node_modules/@editorjs/marker/
 router.use('/editorjs/paragraph.js', express.static('node_modules/@editorjs/paragraph/dist/bundle.js'))
 router.use('/editorjs/underline.js', express.static('node_modules/@editorjs/underline/dist/bundle.js'))
 router.use('/editorjs/inline-code.js', express.static('node_modules/@editorjs/inline-code/dist/bundle.js'))
-router.use('/hexnut/client', express.static('node_modules/hexnut-client/src'))
 
 module.exports = router;
 
